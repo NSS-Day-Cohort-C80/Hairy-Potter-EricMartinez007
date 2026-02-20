b5db684ac3fafcd474127c5c60021c90e934c334
Eric Martinez <ericmartinez1007@gmail.com>
2026-02-20 11:35:49 -0600

    Added makePottery Function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,21 @@
 // Imports go first
-
+import { makePottery } from './PotteryWheel.js'
 
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
+let resurrectionStone = makePottery("resurrection stone", 3, 4)
+let goblet = makePottery("goblet", 5, 8)
+let hedwigStatue = makePottery("hedwig statue", 2, 3)
+let houseKey = makePottery("house key", 4, 6)
+let wandHolder = makePottery("wand holder", 6, 9)
 
 
 // Fire each piece of pottery in the kiln
-
+console.log(resurrectionStone)
+console.log(goblet)
+console.log(hedwigStatue)
+console.log(houseKey)
+console.log(wandHolder)
 
 // Determine which ones should be sold, and their price
 
